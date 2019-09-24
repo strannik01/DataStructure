c801a2b0c6712047f0357ac0d6ea4edbf3af4d4e
Juan Pablo <jcanepa@efact.pe>
2019-09-24 17:18:52 -0500

    nuevas clases

--- src/main/java/com/dev21/chapter4/generics/ejercicio2/LinkedMap.java
+++ src/main/java/com/dev21/chapter4/generics/ejercicio2/LinkedMap.java
@@ -1,16 +1,18 @@
 package com.dev21.chapter4.generics.ejercicio2;
 
+import java.util.HashMap;
+
 public class LinkedMap implements Map<String, Integer> {
-	
+	java.util.Map<String, Integer> interno = new HashMap<>();
 
 	@Override
 	public void put(String k, Integer v) {
-		
+		interno.put(k, v);
 	}
 
 	@Override
 	public Integer get(String k) {
-		return null;
+		return interno.get(k);
 	}
 
 }
--- src/main/java/com/dev21/chapter4/generics/ejercicio2/MapTest.java
+++ src/main/java/com/dev21/chapter4/generics/ejercicio2/MapTest.java
@@ -2,9 +2,11 @@ package com.dev21.chapter4.generics.ejercicio2;
 
 public class MapTest {
 
-//	public static void main(String[] args) {
-//		Map<String, Integer> m = new LinkedMap();
-//		m.
-//	}
+	public static void main(String[] args) {
+		Map<String, Integer> m = new LinkedMap();
+		m.put("sueldo", 10000);
+		int sueldo = m.get("sueldo");
+		System.out.println("Mi sueldo es: " + sueldo);
+	}
 
 }
