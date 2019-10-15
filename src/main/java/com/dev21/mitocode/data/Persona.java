65516cd18e7e689964be84163d6b95d4683c0ba4
juanpablo <juanpablo.canepa21@gmail.com>
2019-10-14 21:32:11 -0500

    Add data

---
+++ src/main/java/com/dev21/mitocode/data/Persona.java
@@ -0,0 +1,44 @@
+package com.dev21.mitocode.data;
+
+public class Persona implements Comparable<Persona> {
+	private int id;
+	private String nombre;
+	private int edad;
+
+	public Persona(int id, String nombre, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public int compareTo(Persona persona) {
+		return this.edad - persona.getEdad();
+	}
+
+}
