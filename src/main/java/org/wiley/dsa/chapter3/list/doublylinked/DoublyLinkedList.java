7966a19db1e04f4640915d29f03b4f150f9f4bca
Juan Pablo <jcanepa@efact.pe>
2019-11-06 21:10:27 -0500

    Add doubly linked list

---
+++ src/main/java/org/wiley/dsa/chapter3/list/doublylinked/DoublyLinkedList.java
@@ -0,0 +1,103 @@
+package org.wiley.dsa.chapter3.list.doublylinked;
+
+public class DoublyLinkedList<E> {
+	private Node<E> header;
+	private Node<E> trailer;
+	private int size;
+	
+	public DoublyLinkedList() {
+		header = new Node<>(null, null, null);	// crea un header
+		trailer = new Node<>(null, header, null);	// trailer está precedido por header
+		header.setNext(trailer);	// el header es seguido por el trailer (trailer es su siguiente elemento
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public E first() {
+		if (isEmpty()) 
+			return null;
+		return header.getNext().getElement();
+	}
+	
+	public E last() {
+		if (isEmpty())
+			return null;
+		return trailer.getPrev().getElement();
+	}
+	
+	public void addFirst(E e) {
+		addBetween(e, header, header.getNext());
+	}
+	
+	public void addLast(E e) {
+		addBetween(e, trailer.getPrev(), trailer);
+	}
+	
+	public E removeFirst() {
+		if (isEmpty())
+			return null;
+		return remove(header.getNext());
+	}
+	
+	public E removeLast() {
+		if (isEmpty())
+			return null;
+		return remove(trailer.getPrev());
+	}
+	
+	private E remove(Node<E> node) {
+		Node<E> predecessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predecessor.setNext(successor);
+		successor.setPrev(predecessor);
+		size--;
+		return node.getElement();
+	}
+
+	private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+		Node<E> newest = new Node<>(e, predecessor, successor);
+		predecessor.setNext(newest);
+		successor.setPrev(newest);
+		size++;
+	}
+
+	private static class Node<E> {
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+
+		public Node(E e, Node<E> p, Node<E> n) {
+			element = e;
+			prev = p;
+			next = n;
+		}
+
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node<E> prev) {
+			this.prev = prev;
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<E> next) {
+			this.next = next;
+		}
+
+	}
+
+}
